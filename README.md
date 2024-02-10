6ddde67080d07be8a8b79f71158036dc3fc082a3
CoderBuilder22 <ahmednasrism@gmail.com>
2024-02-10 00:38:59 +0000

    add same pges
    add same pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# Static E-commerce WebSite
-This project aims to practice fundamental concepts of web development in HTML, CSS, and JavaScript.
---
+++ scriptCart.js
@@ -0,0 +1,4 @@
+function addToCart(destination) {
+    // You can perform additional logic here if needed
+    window.location.href = destination;
+}
